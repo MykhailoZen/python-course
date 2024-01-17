56dddb834db8ff913f3dc35a69f3fd590c7250c8
Viacheslav Riabtsev <mrpumakyiv@gmail.com>
2024-01-17 15:56:11 +0200

    Create a function to find and show the area of a rectangle with known dimensions

---
+++ python/2_basic_syntax/calc_rect_area.py
@@ -0,0 +1,13 @@
+# Define two variables, "length" and "width," and give them the values representing the rectangle's length and width:
+length = 11
+width = 8
+# Compute the area of the rectangle by multiplying the values stored in the "length" and "width" variables:
+area = length * width
+
+# Create a function to find and show the area of a rectangle with known dimensions:
+def calc_rect_area(length, width):
+   # Print the calculated area:
+    print("The area of the rectangle is:", area)
+
+# Example usage:
+calc_rect_area(length, width)
