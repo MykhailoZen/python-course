fe2517c6669f7de8d25043c31aca6dad6ac56252
katerynaleontovych <kateryna.leontovych@gmail.com>
2024-01-18 18:15:33 +0200

    Update home_work_2.py
    better option

--- python/2_basic_syntax/home_work_2.py
+++ python/2_basic_syntax/home_work_2.py
@@ -1,13 +1,8 @@
-length = 10
-width = 5
-area_of_rectangle = length * width
-print(f'Area of rectangle {area_of_rectangle}')
-
-#second option
+#Area of rectangle
 def area_rectangle():
-    length_2 = 10
-    width_2 = 5
-    area_rectangle_2 = length_2 * width_2
-    return f'Area of rectangle {area_rectangle_2}'
+    length = 10
+    width = 5
+    area_rectangle_calculate = length * width
+    return f'Area of rectangle {area_rectangle_calculate}'
 print(area_rectangle())
 
