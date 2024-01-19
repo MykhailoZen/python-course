0e46324cdac5507ca8e3bbaf5c9c28265f0bdff9
Anton Nazarenko <anton.nazarenko@ringteam.com>
2024-01-19 16:36:24 +0200

    add file hw_6 - rectangle_area

---
+++ python/2_basic_syntax/HW_6.py
@@ -0,0 +1,18 @@
+# Practice:
+# Create a function to find and show the area of a rectangle with known dimensions. Here's what you need to do:
+#
+# 1) Define two variables, "length" and "width," and give them the values representing the rectangle's length and width.
+# 2) Compute the area of the rectangle by multiplying the values stored in the "length" and "width" variables.
+# 3) Print the calculated area.
+
+def rectangle_area(x, y):
+    """Calculation of the area of a rectangle"""
+    length, width = x, y
+    area = (length * width)
+    print("Rectangle area: {}".format(area))
+    return (area)
+
+rectangle_area(4, 6)
+
+
+
