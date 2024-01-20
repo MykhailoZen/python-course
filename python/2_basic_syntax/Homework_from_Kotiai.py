50c6ce605e3221bcf0487ba0f16aec28fc7b0d8c
Kotiai <vasyakotyai@gmail.com>
2024-01-20 16:27:07 +0200

    Modified Homework with text comments

--- python/2_basic_syntax/Homework_from_Kotiai.py
+++ python/2_basic_syntax/Homework_from_Kotiai.py
@@ -1,8 +1,10 @@
+# Variant 1
 length = int(input("Please type the length of the rectangle: " ))
 width = int(input("Please type the width of the rectangle: " ))
 rectangle_area = length * width;
 print("Computed rectangle area is equal to %s" % (rectangle_area))
 
+# Variant 2
 length_1 = int(input("Please type the length of the rectangle1: " ))
 width_1 = int(input("Please type the width of the rectangle1: " ))
 def area(x,y):
@@ -10,7 +12,7 @@ def area(x,y):
     return print(("Total rectangle area is equal to %s" % (rec_area)))
 area(length_1,width_1)
 
-
+# Variant 3
 length_2 = int(input("Please type the length of the rectangle2: " ))
 width_2 = int(input("Please type the width of the rectangle2: " ))
 computed_area = lambda x, y: x * y
