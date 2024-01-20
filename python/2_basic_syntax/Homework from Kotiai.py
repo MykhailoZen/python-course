ec2927ffd41b68bb59515f21f6fd0f8c179a0c68
Kotiai <vasyakotyai@gmail.com>
2024-01-20 16:14:11 +0200

    The homework from the Kotiai for Week 6 is done and the code is available in "Homework from Kotiai"

---
+++ python/2_basic_syntax/Homework from Kotiai.py
@@ -0,0 +1,17 @@
+length = int(input("Please type the length of the rectangle: " ))
+width = int(input("Please type the width of the rectangle: " ))
+rectangle_area = length * width;
+print("Computed rectangle area is equal to %s" % (rectangle_area))
+
+length_1 = int(input("Please type the length of the rectangle1: " ))
+width_1 = int(input("Please type the width of the rectangle1: " ))
+def area(x,y):
+    rec_area = x * y
+    return print(("Total rectangle area is equal to %s" % (rec_area)))
+area(length_1,width_1)
+
+
+length_2 = int(input("Please type the length of the rectangle2: " ))
+width_2 = int(input("Please type the width of the rectangle2: " ))
+computed_area = lambda x, y: x * y
+print("Computed rectangle area is equal to %s" % (computed_area(length_2,width_2)))
