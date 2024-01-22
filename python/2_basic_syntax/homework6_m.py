ac5d704f4cbe9c25cfc1bdad19e4ad657485c846
yuliiamaslovska <yuliia.maslovska@ringteam.com>
2024-01-22 21:06:36 +0200

    ymaslovska add homework6_m.py

---
+++ python/2_basic_syntax/homework6_m.py
@@ -0,0 +1,31 @@
+#I
+length = 477
+width = 377
+print (length * width)
+
+#II
+length = 5
+width = 7
+area = length * width
+print (area)
+
+#III
+length = 3
+width = 7
+area = length * width
+print(f"The area of the rectangle with length {length} and width {width} is: {area}")
+
+#IV
+def calculate_rectangle_area(length, width):
+    area = length * width
+
+    print(f"The area of the rectangle with length {length} and width {width} is: {area}")
+
+length = 5
+width = 8
+
+calculate_rectangle_area(length, width)
+
+
+
+
