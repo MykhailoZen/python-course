c02784a946183c8d875c45b2c667b8f9f0f68729
yuriy-sl <ysl_mail@ukr.net>
2024-01-22 21:40:03 +0200

    Added HW#6 code

---
+++ python/2_basic_syntax/area.py
@@ -0,0 +1,7 @@
+def calc_area(length, width):
+    area = length * width
+    print("length is " + str(length))
+    print("width is " + str(width))
+    print("area is " + str(area))
+
+calc_area(12, 34)
