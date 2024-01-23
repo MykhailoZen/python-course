aa89b92e237627d0698d28fef1dae2ee054e43e7
MaksymDevUA <chepurko.mv@gmail.com>
2024-01-23 19:15:39 +0200

    HW number 6, basic syntax

--- python/2_basic_syntax/hw6.py
+++ python/2_basic_syntax/hw6.py
@@ -1,4 +1,8 @@
 def simpleFunct(length, width):
-	print(length*width)
+	area = length * width
+	print("Length: " + str(length))
+	print("Width: " + str(width))
+	print("Area: " + str(area))
+
 
 simpleFunct(10, 10)
