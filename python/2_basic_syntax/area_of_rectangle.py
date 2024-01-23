1f1c1fec942b338a567c0a32daa6008166e104b5
Stanislav Siarkevych <ssyarkevich@hotmail.com>
2024-01-23 12:23:05 +0200

    Created function to calculate rectangle area. Homework #6

---
+++ python/2_basic_syntax/area_of_rectangle.py
@@ -0,0 +1,7 @@
+# Create a function to find and show the area of a rectangle with known dimensions
+def area(length, width):
+    return float(length) * float(width)
+
+
+my_rectangle_area = area(10, 20)
+print("Rectangle area: ", my_rectangle_area)
