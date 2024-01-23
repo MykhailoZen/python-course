3410905baa97b712331e9bde960286ee2e32beed
Serhii Ivanov <0636278046i@gmail.com>
2024-01-23 17:27:11 +0200

    HOMEWORK 6 BASIC SYNTAX

---
+++ python/2_basic_syntax/HW_6.py
@@ -0,0 +1,10 @@
+def rectangle_area(length, width):
+    calculate_area = length * width
+    print("The rectangle area is:", calculate_area)
+    return calculate_area
+
+length = 5
+width = 4
+
+rectangle_area(length, width)
+
