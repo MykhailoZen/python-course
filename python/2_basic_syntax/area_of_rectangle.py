e62ace72a10d6b7650e90e954419e918275dfc56
Stanislav Siarkevych <ssyarkevich@hotmail.com>
2024-01-23 14:37:29 +0200

    Moved print function inside area

--- python/2_basic_syntax/area_of_rectangle.py
+++ python/2_basic_syntax/area_of_rectangle.py
@@ -1,7 +1,7 @@
 # Create a function to find and show the area of a rectangle with known dimensions
 def area(length, width):
-    return float(length) * float(width)
+    calculated_area = float(length) * float(width)
+    print("Rectangle area: ", calculated_area)
 
 
-my_rectangle_area = area(10, 20)
-print("Rectangle area: ", my_rectangle_area)
+area(10, 20)
