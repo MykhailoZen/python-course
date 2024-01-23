0344d4d940ec5d6aea340b884ba0dbf7f63523b8
Ivan Liko <likoivan20@gmail.com>
2024-01-23 14:33:42 +0200

    Added GitHub link to Homework_2.py

---
+++ Homework_2.py
@@ -0,0 +1,7 @@
+def calculate_rectangle_area(length, width):
+    area = length * width
+    print("The area of the rectangle is:", area)
+length = 8
+width = 4
+calculate_rectangle_area(length, width)
+
