eb08fd099cb84a20a8e16c0d18c7f81dd5df0e08
Oleh Pavlenko <oleh.pavlenko@ringteam.com>
2024-01-23 10:31:56 +0200

    little changes on HW#6 solution

--- python/2_basic_syntax/area.py
+++ python/2_basic_syntax/area.py
@@ -1,6 +1,7 @@
 def foo(width, length):
-    print("The area of rectangle - " + str(width * length))
-
+    area = width * length
+    print("The area of rectangle - " + str(area))
+    return area
 
 foo(4, 4)
 foo(4.0, 3)
