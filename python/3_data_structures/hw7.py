1f9819f36b5f9fa3849dd2cf744c5f0782bacf02
yakmchk <yakmchk@gmail.com>
2024-01-26 18:02:25 +0200

    Update hw7.py

--- python/3_data_structures/hw7.py
+++ python/3_data_structures/hw7.py
@@ -1,38 +1,29 @@
-#Create a list named fruits with the following items: 'apple', 'banana', 'cherry', 'potato'.
+# Create a list named fruits with the following items: 'apple', 'banana', 'cherry', 'potato'.
 fruits = ['apple', 'banana', 'cherry', 'potato']
-#Check if 'apple' is in the fruits list and output the result.
+# Check if 'apple' is in the fruits list and output the result.
 for item in fruits:
     if item == 'apple':
         print(item + " is in the list of the fruits")
 
-#Add 'grape' to the fruits list.
+# Add 'grape' to the fruits list.
 fruits.append('grape')
 
-#Remove 'potato' from the fruits list.
+# Remove 'potato' from the fruits list.
 fruits.remove('potato')
 
-#Create a tuple named colors with three colors of your choice.
+# Create a tuple named colors with three colors of your choice.
 colors = ('black', 'yellow', 'white')
-#Calculate the length of the colors tuple and output it.
+# Calculate the length of the colors tuple and output it.
 print(len(colors))
 
-#Create a set named numbers with the numbers from 1 to 3.
+# Create a set named numbers with the numbers from 1 to 3.
 numbers = [1, 2, 3]
-#Add the number 4 to the numbers set.
+# Add the number 4 to the numbers set.
 numbers.append(4)
 print(numbers)
 
-#Create a dictionary named person with the following key-value pairs: 'name' and age.
-person = {'Mira' : 25}
-#Create a new key-value pair in the person dictionary (with new 'name' and age).
+# Create a dictionary named person with the following key-value pairs: 'name' and age.
+person = {'Mira': 25}
+# Create a new key-value pair in the person dictionary (with new 'name' and age).
 person['Serhio'] = 30
 print(person)
-
-
-
-
-
-
-
-
-
