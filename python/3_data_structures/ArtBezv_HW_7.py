db37ff0f5d16b4867781a45cf366a6f01642663b
artem.bezvuhliak <artem.bezvuhliak@gmail.com>
2024-01-28 13:30:06 +0200

    Home_Work_7

---
+++ python/3_data_structures/ArtBezv_HW_7.py
@@ -0,0 +1,16 @@
+fruits = ["apple", "banana", "cherry", "potato"]
+fruits.append("grape")
+fruits.remove("potato")
+print("apple" in fruits)
+print(fruits)
+
+colors = ("green", "red", "blue")
+print(len(colors))
+
+numbers = {1, 2, 3}
+numbers.add(4)
+print(numbers)
+
+person = {"John" : 32, "Eric" : 28, "Lucy" : 25}
+person["Mary"] = 30
+print(person)
