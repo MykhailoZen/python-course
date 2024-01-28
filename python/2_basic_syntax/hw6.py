61f2e5228f17b99328f7dca40b5d04f24332e37d
ksaweb <sersh908@i.ua>
2024-01-29 00:15:59 +0200

    hw6 update

--- python/2_basic_syntax/hw6.py
+++ python/2_basic_syntax/hw6.py
@@ -1,8 +1,8 @@
 length = 5
 width = 10
-
-area = width * length
-
-print('Area is: ' + str(area))
-print('Area is: %s' %(area))
-print (f"Area is: {area}")
+def area_calc(length,width):
+    area = width * length
+    print('Area is: ' + str(area)) #1st method
+    #print('Area is: %s' %(area)) #2nd method
+    #print (f"Area is: {area}") #3rd method
+area_calc(length,width)
