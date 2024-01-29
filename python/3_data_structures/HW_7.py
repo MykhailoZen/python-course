a206c0339aee1a3bf278b57597772146369deb8b
Serhii Ivanov <0636278046i@gmail.com>
2024-01-29 22:03:31 +0200

    "DATA STRUCTURES"

---
+++ python/3_data_structures/HW_7.py
@@ -0,0 +1,38 @@
+# List
+fruits = ["apple", "banana", "cherry", "potato"]
+fruits.append("grape") # Add 'grape' to the fruits list.
+fruits.remove("potato") # Remove 'potato' from the fruits list.
+if "apple" in fruits: #Check if 'apple' is in the fruits list and output the result.
+    print("Apple is in the fruits list")
+else:
+    print("No apple in the fruits list")
+
+
+# Tuple
+colors = ("black", "white", "green")
+# Calculate the length of the colors tuple and output it.
+colors_length = len(colors)
+print(colors_length)
+# or
+print(len(colors))
+
+
+# Set
+numbers = {1, 2, 3}
+numbers.add(4) #Add the number 4 to the numbers set
+
+
+# Dictionary
+person = {"Dmytro":25}
+person["Serhii"] = 32 #Create a new key-value pair in the person dictionary
+
+
+
+
+
+
+
+
+
+
+
