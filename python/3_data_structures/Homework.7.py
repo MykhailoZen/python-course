b501c111385c9bede8d1e9ee05fb9f231b1f3039
ivan.liko <ivan.liko.ai@gmail.com>
2024-01-29 23:49:34 +0200

    change

---
+++ python/3_data_structures/Homework.7.py
@@ -0,0 +1,12 @@
+fruits = ['apple', 'banana', 'cherry', 'potato']
+colors = ('red', 'green', 'blue')
+numbers = {1, 2, 3}
+person = {'name': 'John', 'age': 28}
+fruits.append('grape')
+fruits.remove('potato')
+is_apple_in_fruits = 'apple' in fruits
+print(is_apple_in_fruits)
+colors_length = len(colors)
+print(colors_length)
+numbers.add(4)
+person['new_name'] = 'Ivan'
