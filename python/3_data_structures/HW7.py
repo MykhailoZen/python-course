ab62b9bd9b5dfde7a0e8ba37dfe2297f214376f9
Oleh Pavlenko <oleh.pavlenko@ringteam.com>
2024-01-29 16:11:58 +0200

    HW7 - Data Structures

---
+++ python/3_data_structures/HW7.py
@@ -0,0 +1,21 @@
+fruits = ['apple', 'banana', 'cherry', 'potato']
+colors = ('black', 'white', 'grey')
+numbers = {1, 2, 3}
+person = {"Alex": 10, "Dan": 12, "Ben": 14}
+
+# List
+fruits.append('grape')
+fruits.remove('potato')
+fruits.count('apple')
+print(fruits)
+
+# Tuple
+print(len(colors))
+
+# Set
+numbers.add(4)
+print(numbers)
+
+# Dict
+person['Waughn'] = 15
+print(person)
