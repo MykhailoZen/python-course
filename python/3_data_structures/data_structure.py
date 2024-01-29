a87b130cdb6c6f1f1aecab1916a80a589bb2d28e
katerynaleontovych <kateryna.leontovych@gmail.com>
2024-01-29 15:32:34 +0200

    Create data_structure.py
    HOMEWORK 7
    DATA STRUCTURES

---
+++ python/3_data_structures/data_structure.py
@@ -0,0 +1,23 @@
+# Create a list named fruits with the following items: 'apple', 'banana', 'cherry', 'potato'.
+fruits = ['apple', 'banana', 'cherry', 'potato']
+# Create a tuple named colors with three colors of your choice.
+colors = ('red', 'black', 'blue')
+# Create a set named numbers with the numbers from 1 to 3.
+numbers = {1, 2, 3}
+# Create a dictionary named person with the following key-value pairs: 'name' and age.
+person = {'Kate': 27}
+# Add 'grape' to the fruits list.
+fruits.append('grape')
+# Remove 'potato' from the fruits list.
+fruits.remove('potato')
+# Check if 'apple' is in the fruits list and output the result.
+if "apple" in fruits:
+    print(fruits)
+# Calculate the length of the colors tuple and output it.
+lens_of_colors = len(colors)
+print(lens_of_colors)
+# Add the number 4 to the numbers set.
+numbers.add(4)
+# Create a new key-value pair in the person dictionary (with new 'name' and age).
+person['Luba'] = 45
+print(person)
