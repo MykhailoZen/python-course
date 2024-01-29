58a564b49a2bec6d28df7b20af2266359e41e58c
kalashnyk91 <yevhenii.kalashnyk@ringteam.com>
2024-01-29 18:37:39 +0200

    Practise HOMEWORK 7 added to /3_data_structures/__init__.py

---
+++ python/3_data_structures/__init__.py
@@ -0,0 +1,26 @@
+# Create a list named fruits with the following items: 'apple', 'banana', 'cherry', 'potato'.
+fruits = ['apple', 'banana', 'cherry', 'potato']
+# Create a tuple named colors with three colors of your choice.
+colors = ('white', 'black', 'orange')
+# Create a set named numbers with the numbers from 1 to 3.
+numbers = {1, 2, 3}
+# Create a dictionary named person with the following key-value pairs: 'name' and age.
+person = {
+    "name": "Mike",
+    "age": 33
+}
+# Add 'grape' to the fruits list.
+fruits.append('grape')
+# Remove 'potato' from the fruits list.
+fruits.remove('potato')
+# Check if 'apple' is in the fruits list and output the result.
+if 'apple' in fruits:
+    print("Yes, 'apple' is in the fruits list")
+# Calculate the length of the colors tuple and output it.
+print(len(colors))
+# Add the number 4 to the numbers set.
+numbers.add(4)
+# Create a new key-value pair in the person dictionary (with new 'name' and age).
+person["new_name"] = "John"
+person["new_age"] = 28
+
