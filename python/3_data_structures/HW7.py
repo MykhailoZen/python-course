8c6c918d7827f6100a5bb3856849e3220d2bcb39
Anastasiia Kurach <anastasiia.kurach@Anastasiia.Kurach>
2024-01-30 00:17:24 +0200

    py file for HW7

---
+++ python/3_data_structures/HW7.py
@@ -0,0 +1,12 @@
+fruits = ['apple', 'banana', 'cherry', 'potato']
+colors = ('blue', 'green', 'red')
+numbers = {1, 2, 3}
+person = {'Pavlo': 33}
+fruits.append('grape')
+fruits.remove('potato')
+print("apple" in fruits)
+print(len(colors))
+numbers.add(4)
+person['Anastasiia'] = 29
+
+
