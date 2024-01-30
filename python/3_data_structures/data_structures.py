37412abfa90c8ab0c412315c4152598e7082e120
yuriy-sl <ysl_mail@ukr.net>
2024-01-30 18:52:56 +0200

    added HW#7

---
+++ python/3_data_structures/data_structures.py
@@ -0,0 +1,11 @@
+fruits = ['apple', 'banana', 'cherry', 'potato']
+colors = ('red', 'green', 'blue')
+numbers = {1, 2, 3}
+person = {'Vasa': 25}
+
+fruits.append('grape')
+fruits.remove('potato')
+print('apple' in fruits)
+print(len(colors))
+numbers.add(4)
+person['Kolya'] = 26
