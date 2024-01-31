4011b3be14404627cad6bf8ff8a404bc64029006
Maksim Belinskiy <netvin@gmail.com>
2024-01-31 22:52:10 +0200

    Fix dictionary part of homework

--- python/3_data_structures/maksimbelinskiy.py
+++ python/3_data_structures/maksimbelinskiy.py
@@ -2,12 +2,12 @@ fruits = ['apple', 'banana', 'cherry', 'potato']
 colors = ("red", "green", "blue")
 numbers = {1, 2, 3}
 person = {
-    "name": 'Maks',
-    "age": 38
+    "Maks": 38,
+    "Anton": 30
 }
 fruits.append('grape')
 fruits.remove('potato')
 print('apple' in fruits)
 numbers.add(4)
-person['new_name'] = 'Maksim'
-person['new_age'] = 40
+person['Valera'] = 10
+print(person)
