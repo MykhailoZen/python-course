78125f97996bac2f421411677714833fa8b6387d
Maksim Belinskiy <netvin@gmail.com>
2024-01-31 22:44:32 +0200

    Added file for Homework #7

---
+++ python/3_data_structures/maksimbelinskiy.py
@@ -0,0 +1,13 @@
+fruits = ['apple', 'banana', 'cherry', 'potato']
+colors = ("red", "green", "blue")
+numbers = {1, 2, 3}
+person = {
+    "name": 'Maks',
+    "age": 38
+}
+fruits.append('grape')
+fruits.remove('potato')
+print('apple' in fruits)
+numbers.add(4)
+person['new_name'] = 'Maksim'
+person['new_age'] = 40
