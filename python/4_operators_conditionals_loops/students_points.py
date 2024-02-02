2fc58b70cb3a42b6bbe5a0aeae5929da69eb4ae9
Volodymyr Shkabara <volodymyr.shkabara@Volodymyr.Shkabara>
2024-02-02 16:23:06 +0200

    Students points script

---
+++ python/4_operators_conditionals_loops/students_points.py
@@ -0,0 +1,12 @@
+
+students = {'Alan': 68, 'Roslyn': 48, 'Bertha': 50, 'Sally': 95, 'Rachel': 11, 'John': 95, 'Derek': 82, 'Kevin': 34, 'Michael': 31, 'Richard': 12}
+
+while True:
+    user_input = input("Please, enter student name: ")
+    if user_input == "Stop program":
+        break
+    elif user_input in students:
+        print(f"Student {user_input} has {students[user_input]} points")
+        break
+    else:
+        print(f"There is no student with name {user_input}.")
