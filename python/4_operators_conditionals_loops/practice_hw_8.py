6d2a4ae4665284765859a9efe1a5ad52a5173dda
anton.zhuk <anton.zhuk@squad.ua>
2024-02-06 18:42:07 +0200

    Minor changes

--- python/4_operators_conditionals_loops/practice_hw_8.py
+++ python/4_operators_conditionals_loops/practice_hw_8.py
@@ -1,6 +1,26 @@
-# Dictionary of the all students.
-def students_database():
-    # Dictionary
+# Function for search students in dictionary.
+def search_student(students_names_and_grades: dict, input_student_name: str) -> str:
+    result = True
+    for name, grade in students_names_and_grades.items():
+        if name == input_student_name:
+            result = f'Student {name} has grade {grade} point'
+            break
+        else:
+            result = (f'Student: "{input_student_name}" not found. Try another student name or '
+                      f'enter \'Stop program\' to exit program.')
+
+    return result
+
+
+# Function for stop program.
+def stop_program(input_string):
+    if input_string.lower() == 'stop program':
+        print('Program was stopped...')
+        return exit()
+
+
+# Main program
+def main():
     students_names_and_grades = {
         'Alan': 68,
         'Roslyn': 48,
@@ -14,62 +34,14 @@ def students_database():
         'Richard': 12
     }
 
-    return students_names_and_grades
+    while True:
+        input_string = input('Enter student\'s name: ')
 
-
-# Search students and grades in dictionary.
-def search_students_and_grades(data=None, name_student=None):
-    count = 0
-    result = bool
-
-    for name_student_key in data:
-        if name_student_key == name_student:
-            result = list(data.items())[count]
+        if stop_program(input_string):
             break
         else:
-            result = False
-        count += 1
-    return result
+            search_result = search_student(students_names_and_grades, input_string)
+            print(search_result)
 
 
-# Stop or continue works with program.
-def stop_or_continue_program(entered_value):
-    # Continuing works with program.
-    if entered_value == '':
-        return main_program()
-    # Stop program.
-    elif entered_value == 'stop':
-        print('Program is stopped...')
-        return exit()
-    else:
-        print('Incorrect input. Please try again...')
-        return False
-
-
-def main_program():
-    database = students_database()
-
-    student_name = input('Please enter student name: ')
-    search_in_database = search_students_and_grades(database, name_student=student_name)
-
-    if student_name == '':
-        stop_or_continue_program(student_name)
-    elif bool(search_in_database):
-        search_student_name, search_student_grade = search_in_database
-
-        print('Request was performed successful...')
-        print(f'Result -> Student name: {search_student_name}, Grade: {search_student_grade}')
-
-        while True:
-            continue_or_stop = input('If you want continue tap ENTER or enter \'Stop\' to stop program: ').lower()
-            if stop_or_continue_program(continue_or_stop):
-                break
-    elif not bool(search_in_database):
-        while True:
-            continue_or_stop = input('Student was not found. Please entering another student name tap Enter or '
-                                     'enter \'Stop\' to stop program: ').lower()
-            if stop_or_continue_program(continue_or_stop):
-                break
-
-
-main_program()
+main()
