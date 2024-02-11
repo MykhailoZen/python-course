074078e61a5d81806dbf710cccfc9a82791ece83
yakmchk <yakmchk@gmail.com>
2024-02-11 23:30:45 +0200

    Created a two function. First one prints string and second one calculates the area of the rectangle by the provided arguments

---
+++ python/5_functions/hw_9.py
@@ -0,0 +1,16 @@
+def function():
+    print("Hello World!")
+
+
+function()
+
+
+def rectangle_area(width, length):
+    """This function calculates the area of a rectangle by the provided width and length"""
+    return width * length
+
+
+print(rectangle_area(5,16))
+print(rectangle_area(14, 24))
+print(rectangle_area(20, 30))
+
