d81bf142192e0ea325a4647b8decf1570eb5f45d
Vladyslav Gaidai <vlad.giggs@gmail.com>
2024-02-11 18:18:52 +0200

    5_functions

---
+++ python/5_functions/hw9.py
@@ -0,0 +1,16 @@
+def greeting():
+    """returns 'Hello World!'"""
+    return 'Hello World!'
+
+print(greeting())
+
+
+
+def calculate_rectangle_area(length, width):
+    """returns rectangle area"""
+    mult = length * width
+    return mult
+
+print(calculate_rectangle_area(2, 3))
+print(calculate_rectangle_area(4, 10))
+print(calculate_rectangle_area(94, 58))
