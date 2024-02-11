f5efec26755069c96044746aa363372c6c3387ec
ksaweb <sersh908@i.ua>
2024-02-11 23:55:56 +0200

    HW9 updated

--- python/5_functions/HW9_Functions.py
+++ python/5_functions/HW9_Functions.py
@@ -6,7 +6,12 @@ hello()
 #1.2 Create a function that calculates the area of a rectangle.
 def calculate_rectangle_area(length, width):
     """The function should return the area of the rectangle, which is calculated as multiplication of length and width."""
-    return length * width
-"""Function testing by calling it with various inputs and printing the results."""
-print("Area is:",calculate_rectangle_area(4,3))
-print("Area is:", calculate_rectangle_area(10,5.5))
+    return float(length) * float(width)
+"""Function testing by calling it with various inputs and printing the results:"""
+print("Area is ",calculate_rectangle_area(4,3))
+"""expected output: Area is  12.0"""
+print("Area is ", calculate_rectangle_area(10,5.5))
+"""expected output: Area is  55.0"""
+print("Area is ", calculate_rectangle_area("0.6","5"))
+"""expected output: Area is  3.0"""
+
