3bf446551b3b41dddfcf6763fe58297d04463a7a
artem.bezvuhliak <artem.bezvuhliak@gmail.com>
2024-02-11 10:02:28 +0200

    Home_Work_9

---
+++ python/5_functions/ArtBezv_HW_9.py
@@ -0,0 +1,17 @@
+def simple_func():
+    print("Hello world")
+simple_func()
+
+
+
+def calculate_rectangle_area(length, width):
+    """Function is accept the two arguments as width and length
+       and muptiply them. This will give us a rectangle area."""
+
+    area = length * width
+    print(f"The rectangle area is: {area}")
+    return area
+
+calculate_rectangle_area(7, 5)
+calculate_rectangle_area(4, 8)
+calculate_rectangle_area(11, 6)
