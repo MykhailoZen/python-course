3cd3fd0594e897b517f04cd9c45651944168c365
Anastasiia Kurach <anastasiia.kurach@Anastasiia.Kurach>
2024-02-11 23:34:44 +0200

    py file for HW8

---
+++ python/4_operators_conditionals_loops/HW8.py
@@ -0,0 +1,19 @@
+students = {'Alan': 68, 'Roslyn': 68, 'Bertha': 50, 'Sally': 95, 'Rachel': 11, 'John': 95,
+            'Derek': 82, 'Kevin': 34, 'Michael': 31,'Richard': 12}
+
+print("Hello! This program helps you to find out the student's points. Please enter the name of the student below."
+      "If you want to stop the program enter 'Stop program'")
+
+
+def search_name():
+    name = input("Student name: ")
+    if name in students:
+        print("Student " + name + " has", str(students[name]) + " points")
+    elif name == "Stop program":
+        exit(0)
+    else:
+        print(name + " is not in the list. Please try again.")
+        search_name()
+
+
+search_name()
