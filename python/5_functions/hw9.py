fcf7c632df49ce00ae3d4413709785d7668a6fa8
MaksymDev <53167560+MaksymDevUA@users.noreply.github.com>
2024-02-12 19:18:30 +0200

    Update hw9.py

--- python/5_functions/hw9.py
+++ python/5_functions/hw9.py
@@ -24,23 +24,4 @@ print(calculate_rectangle_area(25, "ohohoh"))
 print(calculate_rectangle_area(3.3, 12))
 print(calculate_rectangle_area(-10, 5))
 
-def calculate_rectangle_area(length, width):
-    if isinstance(length, (int, float)) and isinstance(width, (int, float)):
-        area = length * width
-        if area > 0:
-            return area
-        else:
-            return ("Try again! Incorrect parameters")
-    else:
-        return ("Try again! Incorrect parameters")
-
-
-print(calculate_rectangle_area(45, 2))
-print(calculate_rectangle_area(False, True))
-print(calculate_rectangle_area(22, 4))
-print(calculate_rectangle_area("ohohoh", 25))
-print(calculate_rectangle_area(["a", "f", "i"], ["s", "a", "2"]))
-print(calculate_rectangle_area(25, "ohohoh"))
-print(calculate_rectangle_area(3.3, 12))
-print(calculate_rectangle_area(-10, 5))
 
