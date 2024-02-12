7910d5b0aefcad494a034ed8cae29d48bbf0498d
Stanislav Siarkevych <ssyarkevich@hotmail.com>
2024-02-13 00:39:13 +0200

    Added hw #9. The changes contains a file with two functions: the one that just print 'Hello world' and second - which calculates area of rectangle

---
+++ python/5_functions/home_work9.py
@@ -0,0 +1,26 @@
+def print_string():
+    """The function prints 'Hello world'"""
+    print("Hello World!")
+
+
+def calculate_rectangle_area(length, width):
+    """
+    The function calculates the area of rectangle, calculated as length times width
+    """
+    return length * width
+
+
+# Verify that function calculate_rectangle_area works and returns the correct values. Use a and b variables as sides
+a = 10
+b = 20
+print(f"The area of a rectangle with length {a} and width {b} is", calculate_rectangle_area(a, b))  # Expected 200
+a = 2.5
+b = 4
+print(f"The area of a rectangle with length {a} and width {b} is", calculate_rectangle_area(a, b))  # Expected 10.0
+a = 2.5
+b = 2.5
+print(f"The area of a rectangle with length {a} and width {b} is", calculate_rectangle_area(a, b))  # Expected 6.25
+
+
+
+
