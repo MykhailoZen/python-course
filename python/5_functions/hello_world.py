dd51b92fd37afd8a7843620d8e07fe38c11c2010
Viacheslav Riabtsev <mrpumakyiv@gmail.com>
2024-02-13 16:05:55 +0200

    Hello world

---
+++ python/5_functions/hello_world.py
@@ -0,0 +1,11 @@
+def hello_world():
+    print("Hello World!")
+
+def calculate_rectangle_area(length, width):
+    return length * width
+
+hello_world()
+
+print("Area of rectangle:", calculate_rectangle_area(5, 4))
+print("Area of rectangle:", calculate_rectangle_area(3.5, 2.5))
+print("Area of rectangle:", calculate_rectangle_area(10, 7))
