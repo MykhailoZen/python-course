2f70f7f52218c77ba0e70072675023a428d633d1
ViktorZhulii <zhulii.vik@gmail.com>
2024-02-13 14:11:57 +0200

    functions

---
+++ python/5_functions/func.py
@@ -0,0 +1,17 @@
+def nice():
+    print("Hello World!")
+
+
+nice()
+
+
+def calculate_rectangle_area(length, width):
+    """This function accept two variables to calculate an area"""
+    return length * width
+
+
+print(calculate_rectangle_area(5, 5))
+
+calculate_rectangle_area(5, 4)
+print(calculate_rectangle_area(5, 3))
+print(calculate_rectangle_area(5, 100))
