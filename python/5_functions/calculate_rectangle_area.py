c34232e0d8b31eca3bcfbf6e3c143195054e5830
andriid-fwt <andrii.doroshenko@ringteam.com>
2024-02-13 06:13:22 +0100

    homework 5 calculate_rectangle_area func file aded

---
+++ python/5_functions/calculate_rectangle_area.py
@@ -0,0 +1,17 @@
+def calculate_rectangle_area(length, width):
+    """
+    Calculate rectangle area.
+
+    Parameters:
+    length (float): length of the rectangle
+    width (float): width of the rectangle
+
+    Return:
+    float: The area of the rectangle calculated as length * width.
+    """
+    return length * width
+
+# Function usage examples
+print(calculate_rectangle_area(10, 5))  # Expected output: 50
+print(calculate_rectangle_area(4.5, 2.3))  # Expected output: 10.35
+print(calculate_rectangle_area(7, 3))  # Expected output: 21
