e677f34b0beb87b4b5dfc53c22a9db5712391179
yuriy-sl <ysl_mail@ukr.net>
2024-02-13 16:46:12 +0200

    Added HW#9

---
+++ python/5_functions/rectangle_area.py
@@ -0,0 +1,7 @@
+def calculate_rectangle_area(length, width):
+    """ Function returns the calculated area based on length and width """
+    return length * width
+
+print(f"First area is {calculate_rectangle_area(1, 2)}")  # 2
+print(f"Second area is {calculate_rectangle_area(2, 3)}")  # 6
+print(f"Third area is {calculate_rectangle_area(3, 4)}")  # 12
