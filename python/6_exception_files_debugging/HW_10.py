099aaa43d51017a7b9e8527150b5c03d2fcfe430
Anton Nazarenko <anton.nazarenko@ringteam.com>
2024-02-14 11:03:00 +0200

    added file HW_10

---
+++ python/6_exception_files_debugging/HW_10.py
@@ -0,0 +1,49 @@
+import os
+
+def file_operation(operation_type, file_path, content=None):
+    try:
+        if operation_type == 'read':
+            with open(file_path, 'r') as file:
+                file_content = file.read()
+                print("File content:")
+                print(file_content)
+        elif operation_type == 'write':
+            with open(file_path, 'w') as file:
+                file.write(content)
+                print("Content has been written to the file successfully.")
+        elif operation_type == 'extend':
+            with open(file_path, 'a') as file:
+                file.write(content)
+                print("Content has been appended to the file successfully.")
+        elif operation_type == 'delete':
+            os.remove(file_path)
+            print("File has been deleted successfully.")
+        else:
+            print("Invalid operation type. Please choose 'read', 'write', 'extend', or 'delete'.")
+    except FileNotFoundError as fnf_error:
+        print(fnf_error)
+        print("Please provide a valid file path.")
+    except PermissionError:
+        print("Permission denied. You don't have permission to perform this operation on the file.")
+    except Exception as e:
+        print("An error occurred:", e)
+
+# Example usage:
+if __name__ == "__main__":
+    # Writing to a file
+    file_operation('write', 'example.txt', 'Hello, world!')
+
+    # Extending a file
+    file_operation('extend', 'example.txt', '\nThis is a new line.')
+
+    # Reading from a file
+    file_operation('read', 'example.txt')
+
+    # Error handling example - Invalid operation type
+    file_operation('invalid', 'example.txt')
+
+    # Other Exception
+    file_operation('write', 'example.txt', None)
+
+    # Deleting a file
+    file_operation('delete', 'example.txt')
