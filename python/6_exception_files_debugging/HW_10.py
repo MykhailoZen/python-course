d231e9f8df77c9305cfd23e3a930bbecc606b92b
Anton Nazarenko <anton.nazarenko@ringteam.com>
2024-02-15 14:48:37 +0200

    added docstring to HW_10, addet file with results example.txt

--- python/6_exception_files_debugging/HW_10.py
+++ python/6_exception_files_debugging/HW_10.py
@@ -8,9 +8,6 @@
 # the content of the file should be displayed to the user.
 # Add error handling (try to handle all common input errors).
 
-
-import os
-
 def check_content(content):
     if content != None:
         return True
@@ -35,12 +32,22 @@ def extend_file(file_path, content):
             file.write(str(content) + '\n')
             print("Content has been appended to the file successfully.")
 
-def delete_file(file_path):
-    os.remove(file_path)
-    print("File has been deleted successfully.")
+def clear_file(file_path):
+    with open(file_path, 'w') as file:
+        print("The file was successfully cleared.")
+        pass
 
 
+# Main program
 def file_operation(operation_type, file_path, content=None):
+    """
+    This function allows you to perform file operations such as 'read', 'write', 'extend', or 'clear'.
+    The function takes 2 required (operation_type, file_path) arguments and 1 optional one (content).
+    :param operation_type: 'read', 'write', 'extend', or 'clear'
+    :param file_path: path to the file we want to work with
+    :param content: the data we want to write to the file
+    :return: returns nothing, only prints the result of the operation
+    """
     try:
         if operation_type == 'read':
             read_file(file_path)
@@ -48,10 +55,10 @@ def file_operation(operation_type, file_path, content=None):
             write_file(file_path, content)
         elif operation_type == 'extend':
             extend_file(file_path, content)
-        elif operation_type == 'delete':
-            delete_file(file_path)
+        elif operation_type == 'clear':
+            clear_file(file_path)
         else:
-            raise ValueError("Invalid operation type. Please choose 'read', 'write', 'extend', or 'delete'.")
+            raise ValueError("Invalid operation type. Please choose 'read', 'write', 'extend', or 'clear'.")
     except FileNotFoundError as fnf_error:
         print(fnf_error)
         print("Please provide a valid file path.")
@@ -77,8 +84,8 @@ if __name__ == "__main__":
     # Reading from a file:
     file_operation('read', 'example.txt')
 
-    # Deleting a file:
-    file_operation('delete', 'example.txt')
+    # Clearing a file:
+    file_operation('clear', 'example.txt')
 
     # Error handling example - Invalid operation type:
     file_operation('invalid', 'example.txt')
@@ -93,8 +100,7 @@ if __name__ == "__main__":
     file_operation('extend', 12, 'This is a new line.')
 
     # Error handling example - FileNotFoundError:
-    file_operation('delete', 'example.txt') # not checking FileNotFoundError if the file has not been deleted previously
-    file_operation('read', 'example.txt')
+    file_operation('read', 'missing_file.txt')
 
     # Error handling example - PermissionError:
     # Precondition: chmod ugo=r test_read-only_file.txt
