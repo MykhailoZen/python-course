4de9ba27388c95ccee041e510e29daaf35efce1a
Anton Nazarenko <anton.nazarenko@ringteam.com>
2024-02-19 14:04:51 +0200

    refactoring the function so that the file is opened in only one place in HW_10

--- python/6_exception_files_debugging/HW_10.py
+++ python/6_exception_files_debugging/HW_10.py
@@ -8,100 +8,76 @@
 # the content of the file should be displayed to the user.
 # Add error handling (try to handle all common input errors).
 
-def check_content(content):
-    if content != None:
-        return True
-    else:
-        raise TypeError("Writing data must not be None")
 
-def read_file(file_path):
-    with open(file_path, 'r') as file:
-        file_content = file.read()
-        print("File content:")
-        print(file_content)
-
-def write_file(file_path, content):
-    if check_content(content):
-        with open(file_path, 'w') as file:
-            file.write(str(content) + '\n')
-            print("Content has been written to the file successfully.")
-
-def extend_file(file_path, content):
-    if check_content(content):
-        with open(file_path, 'a') as file:
-            file.write(str(content) + '\n')
-            print("Content has been appended to the file successfully.")
-
-def clear_file(file_path):
-    with open(file_path, 'w') as file:
-        print("The file was successfully cleared.")
-        pass
-
-
-# Main program
 def file_operation(operation_type, file_path, content=None):
     """
-    This function allows you to perform file operations such as 'read', 'write', 'extend', or 'clear'.
+    This function allows you to perform file operations such as 'creation', 'read', 'write', 'extend'.
     The function takes 2 required (operation_type, file_path) arguments and 1 optional one (content).
-    :param operation_type: 'read', 'write', 'extend', or 'clear'
+    :param operation_type: 'r', 'r+', 'a+', 'w+', 'a', 'w', 'x'
     :param file_path: path to the file we want to work with
     :param content: the data we want to write to the file
     :return: returns nothing, only prints the result of the operation
     """
+    operation = ('r', 'r+', 'a+', 'w+', 'a', 'w', 'x')
+
     try:
-        if operation_type == 'read':
-            read_file(file_path)
-        elif operation_type == 'write':
-            write_file(file_path, content)
-        elif operation_type == 'extend':
-            extend_file(file_path, content)
-        elif operation_type == 'clear':
-            clear_file(file_path)
-        else:
-            raise ValueError("Invalid operation type. Please choose 'read', 'write', 'extend', or 'clear'.")
+        with open(file_path, operation_type) as file:
+            if operation_type in operation[:4] and content == None:
+                file.seek(0)
+                file_content = file.read()
+                print("File content:")
+                print(file_content)
+            elif operation_type in operation[1:] and content:
+                file.write(str(content) + '\n')
+                print("Content has been written to the file successfully.")
+            else:
+                print('You do not specify the data to write to the file or something else went wrong.') # OK
+    except ValueError as value_error:
+        print(f"{value_error}. Invalid operation type. Please choose 'r', 'r+', 'a+', 'w+', 'a', 'w', 'x'.") # OK
     except FileNotFoundError as fnf_error:
-        print(fnf_error)
-        print("Please provide a valid file path.")
+        print(f"{fnf_error}. Please provide a valid file path.") # OK
     except PermissionError:
-        print("Permission denied. You don't have permission to perform this operation on the file.")
-    except TypeError as type_error:
-        print("TypeError occurred:", type_error)
+        print("Permission denied. You don't have permission to perform this operation on the file.") # OK
+    except FileExistsError as fileexisterror:
+        print(f'{fileexisterror}. Create a new file so as not to lose data in {file_path}.') # OK
     except OSError as ose_error:
-        print("OSError occurred:", ose_error)
+        print(f"OSError occurred: {ose_error}") # OK
     except Exception as err:
-        print("An error occurred:", err)
+        print(f"An error occurred: {err}")
 
 # Example usage:
 if __name__ == "__main__":
     # Writing to a file:
-    file_operation('write', 'example.txt', 'Hello, world!')
+    file_operation('w', 'example.txt', 'Hello, world!')
+    file_operation('w+', 'example.txt', 1000)
 
     # Extending a file:
-    file_operation('extend', 'example.txt', 'This is a new line.')
-    file_operation('extend', 'example.txt', ['f', 'd', 's'])
-    file_operation('extend', 'example.txt', 1000)
+    file_operation('a', 'example.txt', 'This is a new line.')
+    file_operation('a+', 'example.txt', ['f', 'd', 's'])
 
     # Reading from a file:
-    file_operation('read', 'example.txt')
+    file_operation('r', 'example.txt')
+    file_operation('r+', 'example.txt')
 
-    # Clearing a file:
-    file_operation('clear', 'example.txt')
+    # Creating file and writing:
+    file_operation('x', 'example.txt', 'File was created!')
+    file_operation('x', 'example_x.txt', 'File was created!')
 
-    # Error handling example - Invalid operation type:
+    # Error handling example - Invalid operation type: OK
     file_operation('invalid', 'example.txt')
 
-    # Error handling example - Invalid data type to write to file:
-    file_operation('write', 'example.txt', None)
-    file_operation('write', 'example.txt')
+    # Error handling example - Invalid data type to write to file: OK
+    file_operation('w', 'example.txt', None)
+    file_operation('a', 'example.txt')
 
-    # Error handling example - Bad file descriptor:
-    file_operation('read', 10)
-    file_operation('write', 11, 'Hello, world!')
-    file_operation('extend', 12, 'This is a new line.')
+    # Error handling example - Bad file descriptor: OK
+    file_operation('r', 10)
+    file_operation('w', 11, 'Hello, world!')
+    file_operation('a', 12, 'This is a new line.')
 
-    # Error handling example - FileNotFoundError:
-    file_operation('read', 'missing_file.txt')
+    # Error handling example - FileNotFoundError: OK
+    file_operation('r', 'missing_file.txt')
 
-    # Error handling example - PermissionError:
+    # Error handling example - PermissionError: OK
     # Precondition: chmod ugo=r test_read-only_file.txt
-    file_operation('write', 'test_read-only_file.txt', 'Hello, world!')
+    file_operation('w', 'test_read-only_file.txt', 'Hello, world!')
