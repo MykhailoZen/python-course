21ca5123f18c32cfea83d038075bd94c7176fdb7
Aleksandr Knyazev <aleksandr.knyazev@Aleksandr.Knyazev>
2024-02-20 14:37:31 +0200

    added HW#10

--- python/6_exception_files_debugging/HW#10.py
+++ python/6_exception_files_debugging/HW#10.py
@@ -1,21 +1,19 @@
 def file_operation(operation_type, file_path, file_content=None):
     try:
-        if operation_type == "read":
-            with open(file_path, "r") as file:
+        with open(file_path, "a" if operation_type == "append" else "w" if operation_type == "write" else "r") as file:
+            if operation_type == "read":
                 content = file.read()
                 print(content)
-        elif operation_type == "write":
-            with open(file_path, "w") as file:
+            elif operation_type == "write":
                 file.write(file_content)
-                print("test123 added to a test.txt")
-        elif operation_type == "append":
-            with open(file_path, "a") as file:
+                print("test123 added to test.txt")
+            elif operation_type == "append":
                 if file_content:
                     file_content = "\n" + file_content if not file_content.startswith('\n') else file_content
                 file.write(file_content)
-                print("new line added to test.txt")
-        else:
-            print("Invalid operation type.")
+                print("new line added to test.123")
+            else:
+                print("Invalid operation.")
 
     except FileNotFoundError:
         print("File not found")
