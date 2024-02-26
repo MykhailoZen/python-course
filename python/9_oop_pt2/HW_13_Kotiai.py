2b0725f27b5f7476affef293128955442574965f
Kotiai <vasyakotyai@gmail.com>
2024-02-26 19:13:26 +0200

    Updated HW_13 from Kotiai

--- python/9_oop_pt2/HW_13_Kotiai.py
+++ python/9_oop_pt2/HW_13_Kotiai.py
@@ -43,45 +43,7 @@ with OperFiles("r", 'file_Kotiai_6.txt', 'text line e') as file_operation:
     file_operation.operation()
 
 
-"""Homework for week 9 rebased to class"""
 
-class Area():
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
-
-    def calculate_rectangle_area(self):
-        """2. This function should return the area of the rectangle,
-        which is calculated as multiplication of length and width:"""
-        rec_area = float(self.length) * float(self.width)
-        print("Total rectangle area is equal to")
-        return rec_area
-
-ar = Area(4, 5)
-print(ar.calculate_rectangle_area())
-
-
-""" Homework for week 11 rebased to class"""
-class LambdaFunc():
-    def __init__(self, dic_origin='', number_list=''):
-        self.dic_origin = dic_origin
-        self.number_list = number_list
-    def re(self):
-        dic_flip = {x: y for y, x in self.dic_origin.items()}
-        print(dic_flip)
-
-    def copy_list(self):
-        cp_number_list = [x for x in self.number_list]
-        print(cp_number_list)
-
-l = [x for x in range(10)]
-d = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
-la = LambdaFunc(d, '')
-la.re()
-
-l = [x for x in range(10)]
-cp_num = LambdaFunc('', l)
-cp_num.copy_list()
 
 
 
