828e484255f973776a827d825deae5ad19e0dd58
Maksim Belinskiy <netvin@gmail.com>
2024-02-27 19:27:48 +0200

    Homework #11. Belinskiy Maksim

--- python/7_lambda_functions/maksimbelinskiy.py
+++ python/7_lambda_functions/maksimbelinskiy.py
@@ -20,5 +20,12 @@ mult_result = reduce(lambda a, b: a * b, multi_list)
 
 
 #Given a list of lists (e.g. [[5, 4, 7], [8, 9, 6], [7, 2, 4]]), write a list comprehension that produces a single list of all items (e.g. [5, 4, 7, 8, 9, 6, 7, 2, 4]).
+list_of_lists = [[5, 4, 7], [8, 9, 6], [7, 2, 4]]
+flat_list = [item for sublist in list_of_lists for item in sublist]
 
 #Using a dict comprehension flip the dictionary (make keys from values and vice versa). For example: {'a': 1, 'b': 2, 'c': 3} -> {1: 'a', 2: 'b', 3: 'c'}.
+original_dict = {'a': 1, 'b': 2, 'c': 3}
+flipped_dict = {value: key for key, value in original_dict.items()}
+
+
+#Honestly I don't understand how it works in 6 out of 7 of answers I used chatGPT
