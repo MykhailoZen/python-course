1d5b2550d627af8144f1e4db3634534bc6114945
Anton Nazarenko <anton.nazarenko@ringteam.com>
2024-03-01 02:16:00 +0200

    added checking for instance creation and testing methods HW_12

--- python/8_oop_pt1/HW_12.py
+++ python/8_oop_pt1/HW_12.py
@@ -3,7 +3,7 @@
 # Create a Python file with the next classes:
 # 1) Create the class "Zoo" with a protected list named "animals"
 # 2) Add methods for adding and removing elements from the list.
-# 3) Create classes for Wolf, Lion, Bioson, Parrtot, and Goose
+# 3) Create classes for Wolf, Lion, Bison, Parrot, and Goose
 # a) Each class should have fields: name(str), id(int) and constructor.
 # b) Each class should have the method "play_sound()" which returns the string with the sound of the animal
 # (Use "Roar" for wolf and lion, "Moo" for bison, and "honk" for goose and parrot).
@@ -24,35 +24,19 @@ class Zoo:
         try:
             self.__animals.remove(animal)
         except ValueError:
-            print(f"Error: {animal} not found in the zoo.")
+            print(f"Error: {animal} - not found in the zoo.")
 
     def __str__(self):
-        return f"Zoo list contain{self.__animals}"
-
-# Example usage class Zoo:
-# if __name__ == "__main__":
-#     # Creating an Instance of a Class
-#     animal1 = Zoo()
-#     # Adding a valid element
-#     animal1.add_animal("wolf")
-#     animal1.add_animal("tiger")
-#     # Adding an incorrect element
-#     animal1.add_animal(10)
-#     # Removing a Present Element
-#     animal1.remove_animal("wolf")
-#     # Removing a missing element
-#     animal1.remove_animal("parrot")
-#     # Displaying the contents of a list
-#     print(animal1)
+        return f"Zoo list contain: {self.__animals}"
 
 
 class Animals:
-    animal_count = 0
+    __animal_count = 0
 
     def __init__(self, name):
         self.name = name
-        Animals.animal_count += 1
-        self.id = Animals.animal_count
+        Animals.__animal_count += 1
+        self.id = Animals.__animal_count
 
     def __str__(self):
         return f"{self.name} has id: {self.id}"
@@ -77,7 +61,7 @@ class Bison(Animals):
     def play_sound(self, sound="Moo"):
         return super().play_sound(sound)
 
-class Parrtot(Animals):
+class Parrot(Animals):
     def play_sound(self, sound="Honk"):
         return super().play_sound(sound)
 
@@ -85,11 +69,40 @@ class Goose(Animals):
     def play_sound(self, sound="Honk"):
         return super().play_sound(sound)
 
-gray_wolf = Wolf("Gray_W")
 
-# Example usage class Zoo:
+
 if __name__ == "__main__":
-    print(repr(gray_wolf))
-    print(str(gray_wolf))
-    print(gray_wolf)
+    # Example usage class Zoo:
+    print("Example usage class Zoo:")
+    # Creating an Instance of a Class
+    animal1 = Zoo()
+    # Adding a valid element
+    animal1.add_animal("wolf")
+    animal1.add_animal("tiger")
+    # Adding an incorrect element
+    animal1.add_animal(10)
+    # Removing a Present Element
+    animal1.remove_animal("wolf")
+    # Removing a missing element
+    animal1.remove_animal("parrot")
+    # Displaying the contents of a list
+    print(animal1)
+
+    # Example usage classes Wolf, Lion, Bison, Parrot, and Goose:
+    print("Example usage classes Wolf, Lion, Bison, Parrot, and Goose:")
+    # Creating an Instance of a Class
+    gray_wolf = Wolf("Gray_Wolf")
+    yellow_lion = Lion("Yellow_Lion")
+    blue_bison = Bison("Blue_Bison")
+    green_parrot = Parrot("Green_Parrot")
+    white_goose = Goose("White_Goose")
+    # check method "play_sound()" which returns the string with the sound of the animal
     print(gray_wolf.play_sound())
+    print(blue_bison.play_sound())
+    print(green_parrot.play_sound())
+    # method which represents the current class object as a string to the developer
+    print(repr(gray_wolf))
+    print(repr(white_goose))
+    # method which represents the current class object as a string to the user
+    print(str(yellow_lion))
+    print(blue_bison)
