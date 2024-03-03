a9e964e4da4d894e54f2398dbfcebce5265cc5ee
katerynaleontovych <kateryna.leontovych@gmail.com>
2024-03-03 13:16:13 +0200

    HM12

---
+++ python/8_oop_pt1/exemple.txt.py
@@ -0,0 +1,94 @@
+class Worker:
+
+    def __init__(self, id):
+        self.id = id
+
+
+    def print_id(self):
+        print(f"id: {self.id}")
+
+
+worker_a = Worker(1)
+#print(worker_a.id)
+#1
+worker_a.id = 5
+#print(worker_a.id)
+# 5
+worker_b = Worker(2)
+#print(worker_b.id)
+#2
+
+
+class Factory:
+
+    def __init__(self, *args):
+        self.workers = args
+        # self.factory_modes = kwargs
+
+
+factory = Factory(worker_a, worker_a)
+# print(factory.workers)
+# factory.workers = [worker_b]
+
+
+# static vars
+class Car:
+    cars_count = 0
+
+    def __init__(self, plate_id):
+        self.plate_id = plate_id
+        Car.cars_count += 1
+        self.factory_id = Car.cars_count
+
+
+car_a = Car("GH-01")
+car_b = Car("HW-03")
+car_a.cars_count = 1000
+print(car_a.cars_count)
+#2
+car_b.cars_count
+#2
+
+
+# print(car_b.cars_count)
+
+# static method
+class DMV:
+    car_register = {}
+
+    @staticmethod
+    def register_car(car):
+        DMV.car_register[car.plate_id] = car.factory_id
+
+DMV.register_car(car_b)
+
+dmv_a = DMV()
+#dmv_a.register_car()
+
+# encapsulation
+class PoliceCar:
+
+    def __init__(self, weapon, plate_id):
+        self.__weapon = weapon
+        self.plate_id = plate_id
+
+
+
+
+    def is_weapon_inside(self):
+        return self.__weapon is not None
+
+    def get_weapon(self):
+        if self.__weapon is None:
+            self.__weapon = "pistol"
+        return self.__weapon
+
+    def set_weapon(self, weapon):
+        if isinstance(str, weapon):
+            self.__weapon = weapon
+        else:
+           self.__weapon = 'pistol'
+
+
+police_car = PoliceCar("rifle", "PD-01")
+print(police_car.is_weapon_inside())
---
+++ python/8_oop_pt1/hm_12.py
@@ -0,0 +1,132 @@
+"""
+Create a Python file with the next classes:
+
+1) Create the class "Zoo" with a protected list named "animals"
+2) Add methods for adding and removing elements from the list.
+3) Create classes for Wolf, Lion, Bioson, Parrtot, and Goose
+    a) Each class should have fields: name(str), id(int) and constructor.
+    b) Each class should have the method "play_sound()" which returns the string with the sound of the animal
+    (Use "Roar" for wolf and lion, "Moo" for bison, and "honk" for goose and parrot).
+    c) Create method which represents the current class object as a string e.g. output in print command should be like
+    "class Lion name Alex id 2".
+"""
+
+
+class Zoo:
+    __animals = []
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def add_animal(cls, animal):
+        cls.__animals.append(animal)
+
+    @classmethod
+    def remove_animal(cls, animal):
+        try:
+            cls.__animals.remove(animal)
+        except ValueError as Val:
+            print(f'This {animal} not in the list of animals. Error: {Val}')
+        except Exception as e:
+            return 'An error occurred: ' + str(e)
+
+    @classmethod
+    def get_list_of_animals(cls):
+        return f'All animals in the zoo: {cls.__animals}'
+
+
+class Wolf:
+
+    def __init__(self, name: str, id: int):
+        self.name = name
+        self.id = id
+
+    def play_sound(self):
+        return f'says - Roar'
+
+    def __str__(self):
+        return f'class {self.__class__.__name__}, name {self.name}, with id {self.id},{self.play_sound()}'
+
+
+class Lion:
+
+    def __init__(self, name: str, id: int):
+        self.name = name
+        self.id = id
+
+    def play_sound(self):
+        return f'says - Roar'
+
+    def __str__(self):
+        return f'class {self.__class__.__name__}, name {self.name}, with id {self.id},{self.play_sound()}'
+
+
+class Bison:
+
+    def __init__(self, name: str, id: int):
+        self.name = name
+        self.id = id
+
+    def play_sound(self):
+        return f'says - Moo'
+
+    def __str__(self):
+        return f'class {self.__class__.__name__}, name {self.name}, with id {self.id},{self.play_sound()}'
+
+
+class Parrot:
+
+    def __init__(self, name: str, id: int):
+        self.name = name
+        self.id = id
+
+    def play_sound(self):
+        return f'says - honk'
+
+    def __str__(self):
+        return f'class {self.__class__.__name__}, name {self.name}, with id {self.id},{self.play_sound()}'
+
+
+class Goose:
+
+    def __init__(self, name: str, id: int):
+        self.name = name
+        self.id = id
+
+    def play_sound(self):
+        return f'says - honk'
+
+    def __str__(self):
+        return f'class {self.__class__.__name__}, name {self.name}, with id {self.id},{self.play_sound()}'
+
+
+if __name__ == "__main__":
+    # verification class Zoo
+    Zoo.add_animal("Cat")
+    Zoo.add_animal('Dog')
+    Zoo.add_animal('Crocodile')
+    Zoo.add_animal('Tiger')
+    print(Zoo.get_list_of_animals())
+
+    Zoo.remove_animal('D')
+    print(Zoo.get_list_of_animals())
+
+    Zoo.remove_animal('Crocodile')
+    print(Zoo.get_list_of_animals())
+
+    # verification classes Wolf, Lion, Bison, Parrot, Goose
+    wolf = Wolf('Ivan', 5478)
+    print(wolf)
+
+    lion = Lion('Saran', 4656)
+    print(lion)
+
+    bison = Bison('Babe', 45654)
+    print(bison)
+
+    parrot = Parrot('Сrow', 45654)
+    print(parrot)
+
+    goose = Goose('Baе', 45654)
+    print(goose)
