c4fe4e4a4adfde4d7ebbb1b92aeee131af7f5dfe
anton.zhuk <anton.zhuk@squad.ua>
2024-03-05 17:41:47 +0200

    Minor Fix

--- python/8_oop_pt1/HW_#12_Anton_Zhuk.py
+++ python/8_oop_pt1/HW_#12_Anton_Zhuk.py
@@ -26,77 +26,82 @@ class Zoo:
 
 
 class Wolf:
-    def __init__(self, id_animal: int, name_animal: str):
+    def __init__(self, id_animal: int, name_animal: str) -> None:
         self.name_animal = name_animal
         self.id_animal = id_animal
 
-    def play_sound(self):
+    def play_sound(self) -> str:
         return 'Roar'
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'class {self.__class__.__name__} name {self.name_animal} id {self.id_animal}'
 
 
 class Lion:
-    def __init__(self, id_animal: int, name_animal: str):
+    def __init__(self, id_animal: int, name_animal: str) -> None:
         self.name_animal = name_animal
         self.id_animal = id_animal
 
-    def play_sound(self):
+    def play_sound(self) -> str:
         return 'Roar'
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'class {self.__class__.__name__} name {self.name_animal} id {self.id_animal}'
 
 
 class Bison:
-    def __init__(self, id_animal: int, name_animal: str):
+    def __init__(self, id_animal: int, name_animal: str) -> None:
         self.name_animal = name_animal
         self.id_animal = id_animal
 
-    def play_sound(self):
+    def play_sound(self) -> str:
         return 'Moo'
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'class {self.__class__.__name__} name {self.name_animal} id {self.id_animal}'
 
 
 class Parrot:
-    def __init__(self, id_animal: int, name_animal: str):
+    def __init__(self, id_animal: int, name_animal: str) -> None:
         self.name_animal = name_animal
         self.id_animal = id_animal
 
-    def play_sound(self):
+    def play_sound(self) -> str:
         return 'honk'
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'class {self.__class__.__name__} name {self.name_animal} id {self.id_animal}'
 
 
 class Goose:
-    def __init__(self, id_animal: int, name_animal: str):
+    def __init__(self, id_animal: int, name_animal: str) -> None:
         self.name_animal = name_animal
         self.id_animal = id_animal
 
-    def play_sound(self):
+    def play_sound(self) -> str:
         return 'honk'
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'class {self.__class__.__name__} name {self.name_animal} id {self.id_animal}'
 
 
 zoo = Zoo()
+
+print(f'a. {zoo.get_animals()}')
+
 zoo.add_animals('Horse')
-print(f'1. {zoo.get_animals()}')
+print(f'b. {zoo.get_animals()}')
 
 zoo.extend_animals(['Dog', 'Cat', 'Mouse'])
-print(f'2. {zoo.get_animals()}')
+print(f'c. {zoo.get_animals()}')
 
 zoo.remove_animals('Horse')
-print(f'3. {zoo.get_animals()}')
+print(f'd. {zoo.get_animals()}')
 
 zoo.clear_animals_list()
-print(f'4. {zoo.get_animals()}')
+print(f'e. {zoo.get_animals()}')
+
+print('-' * 35)
 
 wolf_animal = Wolf(1, 'Amur')
 lion_animal = Lion(2, 'Alia')
@@ -105,10 +110,7 @@ parrot_animal = Parrot(4, 'Oscar')
 goose_animal = Goose(5, 'Nikita')
 zoo.extend_animals([wolf_animal, lion_animal, bison_animal, parrot_animal, goose_animal])
 
+counter = 0
 for animal in zoo.get_animals():
-    print(f'{animal} - {animal.play_sound()}')
-
-
-
-
-
+    counter += 1
+    print(f'{counter}. {animal} - {animal.play_sound()}')
