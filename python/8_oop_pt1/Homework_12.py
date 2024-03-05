838d8441a60e203283d25e3e4f55a7f717a1afe6
ivan.liko <ivan.liko.ai@gmail.com>
2024-03-05 17:06:23 +0200

    change

--- python/8_oop_pt1/Homework_12.py
+++ python/8_oop_pt1/Homework_12.py
@@ -1,13 +1,10 @@
 class Zoo:
     def __init__(self):
         self._animals = []
-
     def add_animal(self, animal):
         self._animals.append(animal)
-
     def remove_animal(self, animal):
         self._animals.remove(animal)
-
     def list_animals(self):
         for animal in self._animals:
             print(animal)
@@ -16,13 +13,10 @@ class Animal:
     def __init__(self, name, id):
         self.name = name
         self.id = id
-
     def play_sound(self):
         pass
-
     def __str__(self):
         return f'class {self.__class__.__name__} name {self.name} id {self.id}'
-
 class Wolf(Animal):
     def play_sound(self):
         return "Roar"
@@ -30,7 +24,6 @@ class Wolf(Animal):
 class Lion(Animal):
     def play_sound(self):
         return "Roar"
-
 class Bison(Animal):
     def play_sound(self):
         return "Moo"
