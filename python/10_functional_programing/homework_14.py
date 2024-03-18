dc86a5490f209c6b8d256d8ce5d1162b145bedd5
kalashnyk91 <yevhenii.kalashnyk@ringteam.com>
2024-03-18 14:06:04 +0200

    HW 13

--- python/10_functional_programing/homework_14.py
+++ python/10_functional_programing/homework_14.py
@@ -1,3 +1,6 @@
+from math import factorial
+
+
 # Write a function that takes a list of numbers as input and returns a new list containing the square of each number
 def my_func(nums):
     return [num ** 2 for num in nums]
@@ -8,23 +11,27 @@ print(my_func(numbers))
 
 
 # Write a function that takes a list of numbers as input and returns a new list containing only the even numbers.
-def is_even(x):
+def return_only_even_numbers(x):
     return list(filter(lambda num: num % 2 == 0, x))
 
 
 nums_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(is_even(nums_list))
+print(return_only_even_numbers(nums_list))
 
 
 # Write a function that calculates the factorial of a number using the functional approach.
-def factorial(x):
-    from math import factorial
+def calculate_factorial(x):
     return factorial(x)
 
 
 print(factorial(5))
 
+
 # Capitalize the first letter of each word in a sentence
+def capitalize_sentence(text):
+    return text.title()
+
+
 sentence = "hello world, how are you?"
-capitalized_sentence = sentence.title()
+capitalized_sentence = capitalize_sentence(sentence)
 print(capitalized_sentence)
