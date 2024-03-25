21073cb4b4fb741c5c2be299da76e327f202e266
oleh.pavlenko <oleh.pavlenko@ringteam.com>
2024-03-25 18:03:19 +0200

    HW15. Iterators & generators

---
+++ python/11_iterators_generatos_decorators/HW15.py
@@ -0,0 +1,37 @@
+from itertools import islice
+from time import perf_counter
+from math import factorial
+
+# Create a decorator that logs the time taken by a function to execute.
+def timer(function):
+    def wrapper(args):
+        timer_start = perf_counter()
+        function(args)
+        timer_end = perf_counter()
+        timer_execution = timer_end - timer_start
+        return f"The execution time of {function.__name__} is {timer_execution} seconds."
+    return wrapper
+
+
+@timer
+def random_factorial(n):
+    res = []
+    for i in range(n):
+        res.append(factorial(i))
+    return res
+
+
+final_result = random_factorial(10)
+
+
+# Write a generator function that generates Fibonacci numbers up to a specified limit.
+def fibonacci():
+    previous, current = 0, 1
+    while True:
+        yield current
+        previous, current = current, previous + current
+
+
+f = list(islice(fibonacci(), 0, 15))
+print(f)
+
