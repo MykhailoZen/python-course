484d683323010a9f3ff10d34c3097c7d74d985be
andrii.parfeniuk <and0parf@gmail.com>
2024-04-09 02:21:47 +0300

    hw17 change1

---
+++ python/13_logging/hw17.py
@@ -0,0 +1,17 @@
+"""
+HOMEWORK 17
+
+
+
+1. Add logging to the code implemented in the "Concurrency" module.
+
+There should be at least two logging levels: debug and info.
+All print statements should be replaced with logging.
+Configure logging to print all log messages to the console, and write INFO+ messages to a log file.
+
+2. Configure and run black, isort & flake8 on the files created in the "Concurrency" module.
+
+Add ".flake8" & "pyproject.toml" files with appropriate settings.
+Make sure the configurations are compatible with each other.
+
+"""
