cf548d42a32aec8727a8cb83cb1ca86b76290b74
Oleksandr Lozenko <oleksandr.lozenko@Oleksandr.Lozenko>
2024-04-17 11:03:17 +0300

    added hw18 rev1

--- python/15_imports/flask_app.py
+++ python/15_imports/flask_app.py
@@ -1,15 +1,7 @@
 from flask import Flask, request, jsonify
+from .lib.students_c import Student
 
 app = Flask(__name__)
-
-# In-memory data storage (you can replace this with a database)
-data_storage = {}
-
-
-class Student:
-    def __init__(self, student_name, student_id):
-        self.student_name = student_name
-        self.student_id = student_id
 
 
 @app.route('/home')
@@ -31,7 +23,9 @@ def store_data():
         student_name = data.get('name')
         if student_name is None:
             return jsonify({'error': 'Value not provided'}), 400
-        data_storage[student_id] = student_name
+        #data_storage[student_id] = student_name
+        student = Student(student_name, student_id)
+        Student.student_list.append((student_id, student_name))
         return jsonify({'success': True}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
@@ -41,10 +35,18 @@ def retrieve_data(id):
     """
     Endpoint to retrieve data associated with a given key.
     """
-    if id in data_storage:
-        return jsonify({'id': id, 'name': data_storage[id]}), 200
-    else:
-        return jsonify({'error': 'Id not found'}), 404
+    for student_id, student_name in Student.student_list:
+        if student_id == id:
+            return jsonify({'id': student_id, 'name': student_name}), 200
+    return jsonify({'error': 'Id not found'}), 404
+
+@app.route('/list', methods=['GET'])
+def retrieve_all_data():
+    """
+    Endpoint get all students stored.
+    """
+    all_students = [{'id': student_id, 'name': student_name} for student_id, student_name in Student.student_list]
+    return jsonify(all_students)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ python/15_imports/lib/students_c.py
@@ -0,0 +1,8 @@
+class Student:
+
+    student_list = []
+    def __init__(self, student_name, student_id):
+        self.student_name = student_name
+        self.student_id = student_id
+    def show_list():
+        return Student.student_list
