1962f30806c6e3979c055c1f18887a8f6647b959
yum <153637586+yuliiamaslovska@users.noreply.github.com>
2024-04-17 19:55:04 +0300

    Update __init__.py

--- python/15_imports/HomeworkProjectWithImport/student_app/__init__.py
+++ python/15_imports/HomeworkProjectWithImport/student_app/__init__.py
@@ -1,12 +1,8 @@
 # student_app/__init__.py
 
 # This file can be left empty if no specific initialization is needed.
-# However, you can use it to import modules for convenience:
-# from . import models
-# from . import routes
 
-# Or define variables/functions accessible at the package level:
 # PACKAGE_VERSION = '1.0'
 
-# When `student_app` package is imported, these statements will be executed.
+
 
