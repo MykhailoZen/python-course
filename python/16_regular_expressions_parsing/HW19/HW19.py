50859c2b775b0d4b8a01c9b757619d70088ec49b
nightleks <37367011+nightleks@users.noreply.github.com>
2024-04-23 18:04:26 +0300

    Update HW19.py

--- python/16_regular_expressions_parsing/HW19/HW19.py
+++ python/16_regular_expressions_parsing/HW19/HW19.py
@@ -7,20 +7,21 @@ def get_network_info():
     result = subprocess.run(["ifconfig"], capture_output=True, text=True)
     ifconfig_output = result.stdout
 
-    # Define regular expression pattern
-    pattern = r"inet (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*?ether ([0-9a-fA-F:]{17})"
+    # Define regular expression pattern for IPv6 address and MAC address
+    pattern = r"inet6 ([0-9a-fA-F:]+)%anpi0.*?ether ([0-9a-fA-F:]{17})"
 
     # Use re.findall to find all matches of the pattern in ifconfig output
     matches = re.findall(pattern, ifconfig_output, re.DOTALL)
 
     if matches:
-        return matches[0][1], matches[0][0]
+        
+        return matches[0]
     else:
         return None, None
 
 
 # Get MAC and IP addresses of the main network interface
-mac_address, ip_address = get_network_info()
+ip_address, mac_address = get_network_info()
 
 # Print the MAC and IP addresses
 if mac_address and ip_address:
