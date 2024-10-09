6186b7152d70d1e9eed0f3cc63d998323c5b758b
Volodymyr Atanov <atanov.v@gmail.com>
2024-10-09 14:49:45 +0200

    module 3 done

---
+++ python/3_data_structures/main.py
@@ -0,0 +1,13 @@
+fruits = ['apple', 'banana', 'cherry', 'potato']
+colors = ('red', 'green', 'blue')
+numbers = {1, 2, 3}
+person = {'name': 'John', 'age': 25}
+
+fruits.append('grape')
+fruits.remove('potato')
+print(fruits)
+print('apple' in fruits)
+print(len(colors))
+numbers.add(4)
+person['city'] = 'London'
+print(person)
