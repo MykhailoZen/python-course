fb9ce72119c47bd507b52755228fd7b46b57c36b
nnechay1 <nnechay@amazon.pl>
2024-10-15 18:00:17 +0200

    Home work2

---
+++ python/2_basic_syntax/hw2_nnechay.py
@@ -0,0 +1,7 @@
+# 1) Define two variables, "length" and "width," and give them the values representing the rectangle's length and width.
+# 2) Compute the area of the rectangle by multiplying the values stored in the "length" and "width" variables.
+# 3) Print the calculated area.
+
+length = 5
+width = 6
+print("The calculated area", length * width)
