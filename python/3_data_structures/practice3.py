5f08bfe6db1b94cce8457bea213b4ef70c3899b4
borolha <borolha@amazon.com>
2024-10-23 15:26:52 +0200

    Homerwork 3

---
+++ python/3_data_structures/practice3.py
@@ -0,0 +1,36 @@
+# Create a list named fruits with the following items: 'apple', 'banana', 'cherry', 'potato'.
+# Create a tuple named colors with three colors of your choice.
+# Create a set named numbers with the numbers from 1 to 3.
+# Create a dictionary named person with the following key-value pairs: 'name' and age.
+# Add 'grape' to the fruits list.
+# Remove 'potato' from the fruits list.
+# Check if 'apple' is in the fruits list and output the result.
+# Calculate the length of the colors tuple and output it.
+# Add the number 4 to the numbers set.
+# Create a new key-value pair in the person dictionary (with new 'name' and age).
+# Make sure to perform these operations using the basic capabilities of the data structures
+# (without using comprehensions, lambdas, functions or other advanced features).
+
+fruits = ['apple', 'banana', 'cherry', 'potato']
+
+colors = ('orange', 'green', 'blue')
+numbers = {1,2,3}
+person = {
+    "Olha" : 25
+}
+
+fruits.append('grape')
+fruits.remove('potato')
+print(fruits)
+
+if 'apple' in fruits:
+    print("Yes, there is an apple in the fruits list")
+
+print(len(colors))
+
+numbers.add(4)
+print(numbers)
+
+
+person['Hanna'] = 26
+print(person)
