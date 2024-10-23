39f2a78955d5f3651298b77f0a07911ad91818fe
ramashch <ramashch@amazon.pl>
2024-10-23 15:19:27 +0200

    Homework 3 done

---
+++ python/3_data_structures/ania_mashchenko_data_structures.py
@@ -0,0 +1,12 @@
+fruits = ['apple', 'banana', 'cherry', 'potato']
+colors = ('green', 'blue', 'white')
+numbers = {1, 2, 3}
+person = {'Tom' : 22}
+fruits.append('grape')
+fruits.remove('potato')
+if 'apple' in fruits:
+    print("Yes")
+x = len(colors)
+print(x)
+numbers.add(4)
+person['Jerry'] = 5
