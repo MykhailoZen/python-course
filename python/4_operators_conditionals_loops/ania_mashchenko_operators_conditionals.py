6defb61e5922a2d71d22aa39e3953c74b8e3455a
ramashch <ramashch@amazon.pl>
2024-10-24 20:17:10 +0200

    Homework 4 done

---
+++ python/4_operators_conditionals_loops/ania_mashchenko_operators_conditionals.py
@@ -0,0 +1,15 @@
+# Create a program that will help to find out the student's points.
+# The program prompts you to enter the student's name and, if successful, returns the following message Student <name>
+# has <value> points and complete its execution.
+# If such a name is not in the dictionary, the program should inform about this and offer to enter another name.
+# There should also be a way to end the program by typing Stop program.
+student_grades = {'Alan': 68, 'Roslyn': 48, 'Bertha': 50, 'Sally': 95, 'Rachel': 11,
+                  'John': 95, 'Derek': 82, 'Kevin': 34, 'Michael': 31, 'Richard': 12}
+name = input("Enter the student's name: ")
+if name != "Stop program":
+    while name not in student_grades:
+        name = input("Name is incorrect, print another name: ")
+        if name == "Stop program":
+            break
+    else:
+        print(f"Student {name} has {student_grades[name]} points")
